252fb9b3e8e59b8a4fbea153e24882322dc8cdcc
SharmaTushar1 <tusharsharma2440160@gmail.com>
2024-05-19 15:11:12 +0530

    Add connection request and response functions

--- tracker.js
+++ tracker.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
 Steps to be followed:
@@ -9,33 +9,33 @@ Steps to be followed:
 
 */
 
-const dgram = require('dgram');
-const Buffer = require('buffer').Buffer;
-const urlParse = require('url').parse;
+const dgram = require("dgram");
+const Buffer = require("buffer").Buffer;
+const urlParse = require("url").parse;
+const crypto = require("crypto");
 
 module.exports.getPeers = (torrent, callback) => {
-  const socket = dgram.createSocket('udp4');
-  const url = torrent.announce.toString('utf8');
+  const socket = dgram.createSocket("udp4");
+  const url = torrent.announce.toString("utf8");
 
   // 1. send connect request
   udpSend(socket, buildConnReq(), url);
 
-  socket.on('message', response => {
-    if (respType(response) === 'connect') {
+  socket.on("message", (response) => {
+    if (respType(response) === "connect") {
       // 2. receive and parse connect response
       const connResp = parseConnResp(response);
       // 3. send announce request
       const announceReq = buildAnnounceReq(connResp.connectionId);
       udpSend(socket, announceReq, url);
-    } else if (respType(response) === 'announce') {
+    } else if (respType(response) === "announce") {
       // 4. parse announce response
       const announceResp = parseAnnounceResp(response);
       // 5. pass peers to callback
       callback(announceResp.peers);
     }
-  })
-
-}
+  });
+};
 
 function udpSend(socket, message, rawUrl, callback = () => {}) {
   const url = urlParse(rawUrl);
@@ -47,11 +47,24 @@ function respType(resp) {
 }
 
 function buildConnReq() {
-  // ...
+  const buf = Buffer.alloc(16);
+
+  buf.writeUInt32BE(0x417, 0);
+  buf.writeUInt32BE(0x27101980, 4);
+
+  buf.writeUInt32BE(0, 8);
+
+  crypto.randomBytes(4).copy(buf, 12);
+
+  return buf;
 }
 
 function parseConnResp(resp) {
-  // ...
+  return {
+    action: resp.readUInt32BE(0),
+    transactionId: resp.readUInt32BE(4),
+    connectionId: resp.slice(8),
+  };
 }
 
 function buildAnnounceReq(connId) {
