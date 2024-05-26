ae1585f26c0ea26f5d67e4e137abb677eded341d
SharmaTushar1 <tusharsharma2440160@gmail.com>
2024-05-26 20:48:37 +0530

    add announce request and response

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 "use strict";
 
-const fs = require("fs");
-const bencode = require("bencode");
 const tracker = require("./tracker");
+const torrentParser = require('./torrent-parser');
 
-const torrent = bencode.decode(fs.readFileSync("Test.torrent"));
+const torrent = torrentParser.open('Test.torrent');
 
 tracker.getPeers(torrent, (peers) => {
   console.log("list of peers: ", peers);
